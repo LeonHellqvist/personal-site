994a15f2e28f4ae24eb1bf5bd684a0d536aa46d4
LeonHellqvist <leon.norlin@outlook.com>
2022-07-14 13:51:15 +0200

    Added startpage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+	<div class="container flex flex-wrap justify-between items-center mx-auto">
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+			>Leon Hellqvist</span
+		>
+		<div class="flex md:order-2">
+			<button
+				type="button"
+				class="text-white bg-yellow-600 hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-yellow-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:ring-yellow-700"
+				>Contact</button
+			>
+			<button
+				data-collapse-toggle="mobile-menu-4"
+				type="button"
+				class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				aria-controls="mobile-menu-4"
+				aria-expanded="false"
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/></svg
+				>
+				<svg
+					class="hidden w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+		</div>
+		<div
+			class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+			id="mobile-menu-4"
+		>
+			<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+				<li>
+					<a
+						href="#"
+						class="block py-2 pr-4 pl-3 text-white bg-yellow-500 rounded md:bg-transparent md:text-yellow-500 md:p-0 dark:text-white"
+						aria-current="page">Home</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						>About</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						>Services</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-yellow-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						>Contact</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<!-- End of Navbar -->
+<section>
+  <div class="ml-40 mt-40">
+    <h1 class="font-mono text-9xl text-yellow-500">Leon Hellqvist</h1>
+    <p class="font-mono text-4xl text-yellow-700">Full stack developer!</p>
+  </div>
+  <svg class="pt-32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#eab208" fill-opacity="1" d="M0,224L48,218.7C96,213,192,203,288,208C384,213,480,235,576,224C672,213,768,171,864,128C960,85,1056,43,1152,26.7C1248,11,1344,21,1392,26.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
+</section>
