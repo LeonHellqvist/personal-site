104308bdf4bb0d0b64b11881aec7c00a3a445b2e
LeonHellqvist <leon.norlin@outlook.com>
2022-07-14 09:53:33 +0200

    Static site rendering, tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
+		prerender: {
+			default: true,
+		},
 	}
 };
 
