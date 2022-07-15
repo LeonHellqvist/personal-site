32810071a22c9e0258542c168f9efbb46db1a018
LeonHellqvist <leon.norlin@outlook.com>
2022-07-15 12:14:23 +0200

    Added content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,19 +2,29 @@
 </script>
 
 <svelte:head>
-	<title>Leon Hellqvist</title>
+	<title>Leon Hellqvist - Home</title>
 	<html lang="en" />
+	<meta name="description" content="Leon Hellqvist's personal website. Full-stack developer from Sweden. Here I write a little about myself and my projects!"/>
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://leonhellqvist.com/">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Leon Hellqvist - Personal site">
+	<meta property="og:description" content="Leon Hellqvist's personal website. Full-stack developer from Sweden. Here I write a little about myself and my projects!">
+	<meta property="og:image" content="https://leonhellqvist.com/ogimage.png">
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta property="twitter:domain" content="leonhellqvist.com">
+	<meta property="twitter:url" content="https://leonhellqvist.com/">
+	<meta name="twitter:title" content="Leon Hellqvist - Personal site">
+	<meta name="twitter:description" content="Leon Hellqvist's personal website. Full-stack developer from Sweden. Here I write a little about myself and my projects!">
+	<meta name="twitter:image" content="https://leonhellqvist.com/ogimage.png">
 </svelte:head>
 
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
 		<span class="self-center text-xl font-semibold whitespace-nowrap">Leon Hellqvist</span>
 		<div class="flex md:order-2">
-			<button
-				type="button"
-				class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0"
-				>Contact</button
-			>
 			<button
 				data-collapse-toggle="mobile-menu-4"
 				type="button"
@@ -57,7 +67,7 @@
 				<li>
 					<a
 						href="#"
-						class="block py-2 pr-4 pl-3 text-white bg-red-500 rounded md:bg-transparent md:text-red-500 md:p-0"
+						class="block py-2 pr-4 pl-3 text-white bg-red-800 rounded md:bg-transparent md:text-red-500 md:p-0"
 						aria-current="page">Home</a
 					>
 				</li>
@@ -120,7 +130,7 @@
 			<div
 				class="ml-5 mr-5 mb-6 lg:w-1/4 xl:w-sm xl:mb-0 block p-6 w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
 			>
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Programming languages</h5>
+				<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Programming languages</h3>
 				<ol class="list-disc pl-4 font-normal text-gray-700">
 					<li>TypeScript</li>
 					<li>JavaScript</li>
@@ -134,7 +144,7 @@
 			<div
 				class="ml-5 mr-5 mb-6 lg:w-1/3 xl:w-sm xl:mb-0 block p-6 min-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
 			>
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Most used technologies</h5>
+				<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Most used technologies</h3>
 				<ol class="list-disc pl-4 font-normal text-gray-700">
 					<li>
 						React
@@ -160,7 +170,7 @@
 			<div
 				class="ml-5 mr-5 block lg:w-1/4 xl:w-sm p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
 			>
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Less used technologies</h5>
+				<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Less used technologies</h3>
 				<ol class="list-disc pl-4 font-normal text-gray-700">
 					<li>Tailwind</li>
 					<li>Svelte</li>
@@ -192,9 +202,9 @@
 			<div
 				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-blue-500/20 hover:bg-gray-100"
 			>
-				<h5 class="text-2xl font-bold tracking-tight text-gray-900">Unikum Tools</h5>
+				<h3 class="text-2xl font-bold tracking-tight text-gray-900">Unikum Tools</h3>
 				<p class="text-gray-700">
-					The past I have been working on, and recently finished a web browser extension. I made the
+					In the past I have been working on, and recently finished a web browser extension. I made the
 					extension as a hobby project and to use for school. The extension adds bookmarks to
 					Unikum.net and is able to show both the schedule and food menu.
 					<br />
@@ -206,7 +216,7 @@
 			<div
 				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-green-700/30 hover:bg-gray-100"
 			>
-				<h5 class="text-2xl font-bold tracking-tight text-gray-900">OpenMineBay</h5>
+				<h3 class="text-2xl font-bold tracking-tight text-gray-900">OpenMineBay</h3>
 				<p class="text-gray-700">
 					The past year I have been working on a marketplace that integrades directly with
 					Minecraft. It has a full web interface build with React (NextJS) and a NodeJS backend. It
@@ -218,7 +228,7 @@
 			<div
 				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-pink-700/30 hover:bg-gray-100"
 			>
-				<h5 class="text-2xl font-bold tracking-tight text-gray-900">Hair Salon</h5>
+				<h3 class="text-2xl font-bold tracking-tight text-gray-900">Hair Salon</h3>
 				<p class="text-gray-700">
 					When looking for hair salons for myself I stumbled upon a really poorly designed website
 					from like 1999. I decided to make a new response website for them for free using React
@@ -232,31 +242,37 @@
 </section>
 <section>
 	<svg class="bg-white -mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#111827" fill-opacity="1" d="M0,128L48,133.3C96,139,192,149,288,138.7C384,128,480,96,576,90.7C672,85,768,107,864,138.7C960,171,1056,213,1152,192C1248,171,1344,85,1392,42.7L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-	<div class="container mx-auto lg:-mt-2 xl:-mt-44">
+	<div class="bg-gray-900 w-screen mx-auto -mt-1 lg:-mt-20 xl:-mt-30 2xl:-mt-32">
 		<h2
 			class="text-center text-white font-mono font-semibold text-4xl xl:text-5xl text-black-800 xl:mt-[-50px] pb-10"
 		>
 			Contact
 		</h2>
+		<div class="flex justify-center -mt-4">
+			<div>
+				<a href="https://github.com/LeonHellqvist/">
+					<button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+						<svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
+						Follow me on Github
+					</button>
+				</a>
+				<a href="mailto:leon@leonhellqvist.com">
+					<button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+						<svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"></path></svg>
+						leon@leonhellqvist.com
+					</button>
+				</a>
+				<button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+					<svg class="mr-2 -ml-1 w-6 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"></path></svg>
+					NisseV2#6585
+				</button>
+			</div>
+		</div>
 	</div>
 </section>
-<footer class="p-4 bg-white shadow-2xl md:flex md:items-center md:justify-between md:p-6">
+<footer class="p-4 bg-gray-900 shadow-2xl md:flex md:items-center md:justify-between md:p-6">
 	<span class="text-sm text-gray-500 sm:text-center"
-		>© 2022 <a href="https://hellqvist.dev/" class="hover:underline">Leon Hellqvist</a>. All Rights
+		>© 2022 <a href="https://leonhellqvist.com/" class="hover:underline">Leon Hellqvist</a>. All Rights
 		Reserved.
 	</span>
-	<ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
-		<li>
-			<a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-		</li>
-		<li>
-			<a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-		</li>
-		<li>
-			<a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-		</li>
-		<li>
-			<a href="#" class="hover:underline">Contact</a>
-		</li>
-	</ul>
 </footer>
