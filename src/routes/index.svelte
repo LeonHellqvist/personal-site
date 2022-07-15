fa14694aa4929bb37ab424c690d693b1931b25d7
LeonHellqvist <leon.norlin@outlook.com>
2022-07-15 14:44:52 +0200

    Added content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,31 @@
 <script>
+
+	const currentYear = new Date().getFullYear();
+	
+	/**
+* @param {Date} birthday
+*/
+	function calculateAge(birthday) { // birthday is a date
+   // @ts-ignore
+   const ageDifMs = Date.now() - birthday;
+   const ageDate = new Date(ageDifMs); // miliseconds from epoch
+   return Math.abs(ageDate.getUTCFullYear() - 1970);
+ }
+
+ const yearsSinceBirthday = calculateAge(new Date("2005-01-02"));
+
+ const navOpen = "sm:hidden";
+ const navClosed = "hidden";
+ let navState = navClosed;
+
+ function navBar() {
+	 if (navState === navClosed) {
+		 navState = navOpen;
+	 } else {
+		 navState = navClosed;
+	 }
+ }
+
 </script>
 
 <svelte:head>
@@ -21,93 +48,84 @@
 	<meta name="twitter:image" content="https://leonhellqvist.com/ogimage.png">
 </svelte:head>
 
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
-	<div class="container flex flex-wrap justify-between items-center mx-auto">
-		<span class="self-center text-xl font-semibold whitespace-nowrap">Leon Hellqvist</span>
-		<div class="flex md:order-2">
-			<button
-				data-collapse-toggle="mobile-menu-4"
-				type="button"
-				class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-				aria-controls="mobile-menu-4"
-				aria-expanded="false"
-			>
-				<span class="sr-only">Open main menu</span>
-				<svg
-					class="w-6 h-6"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-						clip-rule="evenodd"
-					/></svg
-				>
-				<svg
-					class="hidden w-6 h-6"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					/></svg
-				>
-			</button>
-		</div>
-		<div
-			class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-			id="mobile-menu-4"
-		>
-			<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-				<li>
-					<a
-						href="#"
-						class="block py-2 pr-4 pl-3 text-white bg-red-800 rounded md:bg-transparent md:text-red-500 md:p-0"
-						aria-current="page">Home</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
-						>Competence</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
-						>Experience</a
-					>
-				</li>
-				<li>
-					<a
-						href="#"
-						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
-						>Contact</a
-					>
-				</li>
-			</ul>
-		</div>
-	</div>
+<nav class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <!-- Mobile menu button-->
+        <button on:click={navBar} type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <!--
+            Icon when menu is closed.
+
+            Heroicon name: outline/menu
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+          <!--
+            Icon when menu is open.
+
+            Heroicon name: outline/x
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+          <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div class="flex-1 flex items-center justify-end mr-6 sm:mr-0 sm:items-stretch sm:justify-start">
+        <div class="flex-shrink-0 flex items-center">
+          <img class="block lg:hidden h-8 w-auto" src="icon.png" alt="Small Logo">
+          <img class="hidden lg:block h-8 w-auto" src="icon.png" alt="Large Logo">
+        </div>
+        <div class="hidden sm:block sm:ml-6">
+          <div class="flex space-x-4">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            <a href="#home" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+
+            <a href="#competence" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Competence</a>
+
+            <a href="#experience" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Experience</a>
+
+            <a href="#contact" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="{navState}" id="mobile-menu">
+    <div class="px-2 pt-2 pb-3 space-y-1">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+      <a href="#home" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</a>
+
+      <a href="#competence" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Competence</a>
+
+      <a href="#experience" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Experience</a>
+
+      <a href="#contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+    </div>
+  </div>
 </nav>
 <!-- End of Navbar -->
-<section class="screen-h">
-	<div class="pt-10 pb-24 lg:ml-16 lg:mt-16 xl:ml-40 xl:mt-40">
-		<h1 class="font-mono text-center lg:text-left text-5xl lg:text-8xl xl:text-9xl text-red-500">
-			Leon Hellqvist
-		</h1>
-		<p class="font-mono text-center lg:text-left text-4xl lg:text-4xl text-red-200 ml-3">
-			Full-stack developer
-		</p>
-		<p class="font-mono text-center lg:text-left text-2xl lg:text-3xl text-red-200 ml-3">
-			Currently 17 years old
-		</p>
+<section class="screen-h" id="home">
+	<div class="pt-20 pb-24 lg:ml-16 lg:mt-16 xl:ml-40 xl:mt-40 flex-col justify-center">
+		<div class="mr-4 sm:ml-0 sm:mr-0">
+			<h1 class="font-mono text-center lg:text-left text-5xl lg:text-8xl xl:text-9xl text-red-500">
+				Leon Hellqvist
+			</h1>
+			<p class="font-mono text-center lg:text-left text-4xl lg:text-4xl text-red-200 sm:ml-3">
+				Full-stack developer
+			</p>
+			<p class="font-mono text-center lg:text-left text-2xl lg:text-3xl text-red-200 sm:ml-3">
+				Currently {yearsSinceBirthday} years old
+			</p>
+		</div>
 	</div>
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"
 		><path
@@ -117,7 +135,7 @@
 		/></svg
 	>
 </section>
-<section class="bg-white mt-[-1px]">
+<section class="bg-white mt-[-1px]" id="competence">
 	<div class="bg-red-500">
 		<h2
 			class="text-center font-mono font-semibold text-4xl xl:text-5xl text-white xl:mt-[-50px] pb-10"
@@ -128,7 +146,7 @@
 			class="container lg:w-5/6 xl:w-4/6 flex flex-wrap justify-center items-center mx-auto"
 		>
 			<div
-				class="ml-5 mr-5 mb-6 lg:w-1/4 xl:w-sm xl:mb-0 block p-6 w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
+				class="ml-5 mr-5 mb-6 lg:w-1/4 xl:w-sm xl:mb-0 block p-6 w-sm bg-white rounded-lg border border-gray-200 shadow-md "
 			>
 				<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Programming languages</h3>
 				<ol class="list-disc pl-4 font-normal text-gray-700">
@@ -142,7 +160,7 @@
 				</ol>
 			</div>
 			<div
-				class="ml-5 mr-5 mb-6 lg:w-1/3 xl:w-sm xl:mb-0 block p-6 min-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
+				class="ml-5 mr-5 mb-6 lg:w-1/3 xl:w-sm xl:mb-0 block p-6 min-w-sm bg-white rounded-lg border border-gray-200 shadow-md "
 			>
 				<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Most used technologies</h3>
 				<ol class="list-disc pl-4 font-normal text-gray-700">
@@ -168,7 +186,7 @@
 				</ol>
 			</div>
 			<div
-				class="ml-5 mr-5 block lg:w-1/4 xl:w-sm p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
+				class="ml-5 mr-5 block lg:w-1/4 xl:w-sm p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md "
 			>
 				<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Less used technologies</h3>
 				<ol class="list-disc pl-4 font-normal text-gray-700">
@@ -191,7 +209,7 @@
 		/></svg
 	>
 </section>
-<section class="w-screen bg-white lg:-mt-16 xl:-mt-32">
+<section class="w-screen bg-white lg:-mt-16 xl:-mt-32" id="experience">
 	<div class="container mx-auto -mt-1">
 		<h2
 			class="text-center font-mono font-semibold text-4xl xl:text-5xl text-black-800 xl:mt-[-50px] pb-10"
@@ -200,7 +218,7 @@
 		</h2>
 		<div class="flex flex-wrap justify-center items-center mx-auto">
 			<div
-				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-blue-500/20 hover:bg-gray-100"
+				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-blue-500/20 "
 			>
 				<h3 class="text-2xl font-bold tracking-tight text-gray-900">Unikum Tools</h3>
 				<p class="text-gray-700">
@@ -208,13 +226,13 @@
 					extension as a hobby project and to use for school. The extension adds bookmarks to
 					Unikum.net and is able to show both the schedule and food menu.
 					<br />
-					<a href="https://github.com/LeonHellqvist/unikum-tools" class="text-blue-500"
+					<a href="https://github.com/LeonHellqvist/unikum-tools" class="text-blue-600"
 						>View on Github</a
 					>
 				</p>
 			</div>
 			<div
-				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-green-700/30 hover:bg-gray-100"
+				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-green-700/30 "
 			>
 				<h3 class="text-2xl font-bold tracking-tight text-gray-900">OpenMineBay</h3>
 				<p class="text-gray-700">
@@ -222,11 +240,11 @@
 					Minecraft. It has a full web interface build with React (NextJS) and a NodeJS backend. It
 					allows you to sell and buy items from other players and works on 100% vanilla servers!
 					<br />
-					<a href="#" class="text-green-500">(Soon public)</a>
+					<a href="#" class="text-green-700">(Soon public)</a>
 				</p>
 			</div>
 			<div
-				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-pink-700/30 hover:bg-gray-100"
+				class="ml-5 mr-5 mb-6 xl:mb-0 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-xl shadow-pink-700/30 "
 			>
 				<h3 class="text-2xl font-bold tracking-tight text-gray-900">Hair Salon</h3>
 				<p class="text-gray-700">
@@ -234,13 +252,13 @@
 					from like 1999. I decided to make a new response website for them for free using React
 					(Gatsby) and Tailwind. They never replied to my mail tho :(
 					<br />
-					<a href="https://github.com/LeonHellqvist/salon" class="text-pink-500">View on Github</a>
+					<a href="https://github.com/LeonHellqvist/salon" class="text-pink-600">View on Github</a>
 				</p>
 			</div>
 		</div>
 	</div>
 </section>
-<section>
+<section id="contact">
 	<svg class="bg-white -mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#111827" fill-opacity="1" d="M0,128L48,133.3C96,139,192,149,288,138.7C384,128,480,96,576,90.7C672,85,768,107,864,138.7C960,171,1056,213,1152,192C1248,171,1344,85,1392,42.7L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 	<div class="bg-gray-900 w-screen mx-auto -mt-1 lg:-mt-20 xl:-mt-30 2xl:-mt-32">
 		<h2
@@ -249,7 +267,7 @@
 			Contact
 		</h2>
 		<div class="flex justify-center -mt-4">
-			<div>
+			<div class="w-min sm:w-fit">
 				<a href="https://github.com/LeonHellqvist/">
 					<button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
 						<svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
@@ -271,8 +289,8 @@
 	</div>
 </section>
 <footer class="p-4 bg-gray-900 shadow-2xl md:flex md:items-center md:justify-between md:p-6">
-	<span class="text-sm text-gray-500 sm:text-center"
-		>© 2022 <a href="https://leonhellqvist.com/" class="hover:underline">Leon Hellqvist</a>. All Rights
+	<span class="text-sm text-gray-200 sm:text-center"
+		>© {currentYear} <a href="https://leonhellqvist.com/" class="hover:underline">Leon Hellqvist</a>. All Rights
 		Reserved.
 	</span>
 </footer>
